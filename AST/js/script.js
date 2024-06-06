53a9e0877f45e971ddfd9b7955c7735726430d75
DevRuanX <160757151+DevRuanX@users.noreply.github.com>
2024-06-06 13:40:30 -0300

    Main

---
+++ AST/js/script.js
@@ -0,0 +1,13 @@
+// MENU HAMBURGER //
+
+const menu = document.querySelector('#btn-menu');
+const boxMenuItens = document.querySelector('.box-menu-itens');
+
+menu.addEventListener('click', (e) =>{
+    e.preventDefault();
+    boxMenuItens.classList.toggle('open')
+    menu.classList.add('lines')
+})
+
+//...//
+
